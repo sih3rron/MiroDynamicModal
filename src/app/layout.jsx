33a73a25dc2a8242ee173015c5e1f45db1b90437
Simon Herron <siherron+github@gmail.com>
2024-12-12 14:36:14 +0000

    Added Vercel SpeedInsights

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Script from 'next/script';
 import { MiroSDKInit } from '../components/SDKInit';
 import '../app/globals.css'
@@ -8,6 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <SpeedInsights />
         <Script src="https://miro.com/app/static/sdk/v2/miro.js" strategy="beforeInteractive" />
         <MiroSDKInit fullscreen={false}/>
         <div id="root">
